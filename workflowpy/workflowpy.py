5f0a862094cbb50c61f6de91f679dcb3d895e96a
Soren Rasmussen <s.c.rasmussen@gmail.com>
2022-02-10 10:44:57 -0700

    added OOP filename type make dealing with data filenames easier

--- workflowpy/workflowpy.py
+++ workflowpy/workflowpy.py
@@ -1,12 +1,17 @@
 import argparse
+from copy import deepcopy
 import datetime
 import subprocess
-# from hydrodartpy import setup_experiment
+import re
+import shutil
 import sys
 import os
+import wrfhydropy
 import yaml
 
 # -- todo ---
+# * update yamls along the way, in advance_ensemble
+# * wrfhydropy
 # * read resource info using CLI
 # * make parse_arguments less brittle
 def exit(message):
@@ -20,6 +25,9 @@ def pwd():
     return os.getcwd()
 def cd(goto):
     os.chdir(goto)
+def check_dir(dir_path):
+    if not os.path.isdir(dir_path):
+        os.makedirs(dir_path)
 def check_path(path):
     if (path[-1] != '/'):
         path += '/'
@@ -39,6 +47,41 @@ def check_wrf_h_path(path: str) -> str:
     return path
 
 
+class filename:
+    def __init__(self, fullpath, dt):
+        self.fullpath = fullpath
+        self.filename = os.path.basename(fullpath)
+        self.previousfilename = ''
+        self.dirname = os.path.basename(fullpath) + '/'
+        reg_s = '[1-9][0-9]{3}[0-1][0-9][0-3][0-9][0-2][0-4]'
+        s = re.search(reg_s, self.filename)
+        self.filebase = self.filename[:s.start()]
+        self.fileend = self.filename[s.end():]
+        date_s = self.filename[s.start():s.end()]
+        self.incrementfilename = self.filename + '.increment'
+        self.date = datetime.datetime.strptime(date_s,'%Y%m%d%H')
+        self.dt = dt
+
+    def advance(self):
+        self.previousfilename = self.filename
+        self.date += self.dt
+        self.filename = self.filebase + \
+            self.date.strftime('%Y%m%d%H') + \
+            self.fileend
+        self.fullpath = self.dirname + self.filename
+        self.incrementfilename = self.filename + '.increment'
+
+    def moveto(self, topath):
+        self.dirname = check_path(topath)
+        shutil.copy(self.fullpath, self.dirname)
+        self.fullpath = self.dirname + self.filename
+
+    def append(self, postfix):
+        self.fullpath += postfix
+        self.filename += postfix
+        self.fileend += postfix
+
+
 class node:
     def __init__(self):
         self.nodes = 1
@@ -55,40 +98,58 @@ class workflow:
         self.cycle()
         self.end()
 
+
     def cycle(self):
         pprint("Starting Cycling Process")
         print(" starting at", self.start_time)
         self.ens_time = self.start_time
         while self.ens_time < self.end_time:
-            self.run_jedi()
+            self.run_filter()
             self.increment_restart()
             self.run_ensemble()
-            self.advance_ensemble()
+            self.advance_model()
         print(" Ending Cycling Process")
+
+
+    def end(self):
+        pprint("Exiting Workflowpy")
+        sys.exittt()
+
 
     def run_ensemble(self):
         pprint('Running ensemble WRF-Hydro')
-        cd(self.jedi_output_dir)
+        # cd(self.jedi_output_dir)
+        cd(self.workflow_work_dir)
         f_name = self.experiment_file_base + \
             self.ens_time.strftime('%H') + '.nc'
         #     f = open(f_name, 'w')
         #     f.close()
         cmd = [self.wrf_h_exe, f_name]
         self.run(cmd)
+        # shutil.copy(self.lsm_filename, self.input_lsm_f_name +'.increment')
+
 
     def increment_restart(self):
         pprint('Incrementing restarts')
-        cd(self.jedi_output_dir)
-        cmd = [self.increment_exe, 'FOO', 'FOO.INC']
+        cd(self.workflow_work_dir)
+        cmd = [self.increment_exe,
+               self.lsm_file.filename,
+               self.lsm_file.incrementfilename]
         self.run(cmd)
 
-    def run_jedi(self):
-        pprint('Running JEDI')
-        cd(self.jedi_working_dir)
-        # ARTLESS: RUN WHERE?
-        cd(self.jedi_output_dir)
+    def run_filter(self):
+        pprint('Running JEDI filter')
+        cd(self.workflow_work_dir)
+        # TODO: RUN WHERE?
+        # cd(self.jedi_output_dir)
         cmd = [self.jedi_exe, self.jedi_yaml]
         self.run(cmd)
+
+        # TODO
+        shutil.copy(self.lsm_file.filename, self.lsm_file.incrementfilename)
+        print("NEED TO MV NEW FILE, UPDATE YAMLS")
+        # sys.exittt()
+
         # sys.exittt()
 # def run_filter(run_dir: pathlib.Path,nproc: int, cmd: str,
 # ):
@@ -104,19 +165,15 @@ class workflow:
 #         raise ValueError('Filter did not return 0')
 
 
-
-    # def run_ensemble(self):
-    #     f_name = self.experiment_file_base + \
-    #         self.ens_time.strftime('%H') + '.nc'
-    #     f = open(f_name, 'w')
-    #     f.close()
-        # TODO: Run Ensemble
-        # print('$', self.command)
-        # subprocess.call([str(self.command)], shell=True)
-
-    def advance_ensemble(self):
-        self.ens_time += self.model_ts
+    def advance_model(self):
+        self.ens_time += self.model_dt
         pprint("Advancing to " + str(self.ens_time))
+        self.lsm_file.advance()
+        shutil.copy(self.lsm_file.previousfilename, self.lsm_file.filename)
+        # self.jedi_yaml = self.setup['experiment']['jedi']['yaml']
+        # f = open(self.jedi_yaml, 'r')
+        # self.jedi_setup = yaml.safe_load(f)
+        # f.close()
 
     def get_resource_info(self):
         pprint("TODO: Obtaining resource info")
@@ -127,22 +184,40 @@ class workflow:
     # make sure everything is in proper format
     # make sure directories exist
     def setup_experiment(self):
-        # make working dir, artless can remove
-        if not os.path.isdir(self.jedi_working_dir):
-            os.mkdir(self.jedi_working_dir)
-        if not os.path.isdir(self.jedi_output_dir):
-            os.mkdir(self.jedi_output_dir)
+        check_dir(self.workflow_work_dir)
+        check_dir(self.jedi_working_dir)
+        check_dir(self.jedi_output_dir)
+
+        # collect starting files
+        self.lsm_file.moveto(self.workflow_work_dir)
+        # shutil.copy(self.input_hydro_f_path, self.workflow_work_dir)
+        shutil.copy(self.jedi_yaml, self.workflow_work_dir)
+        self.jedi_yaml = self.workflow_work_dir + '/' + \
+            os.path.basename(self.jedi_yaml) # TODO: improve
+
+        # update jedi yaml with copied files
+        f = open(self.jedi_yaml, 'r')
+        jedi_setup = yaml.safe_load(f)
+        background = jedi_setup['cost function']['background']
+        background['filename_lsm'] = self.lsm_file.fullpath
+        # background['filename_hydro'] = self.workflow_work_dir + '/' + \
+        #     self.input_hydro_f_name
+        f.close()
+
+        self.write_jedi_yaml(jedi_setup)
+
         # make sure JEDI executable is found
-        exe = False
-        check_exe = self.jedi_build_dir + self.jedi_exe
-        if os.path.exists(check_exe):
-            exe = check_exe
-        check_exe = self.jedi_build_dir + 'bin/' + self.jedi_exe
-        if os.path.exists(check_exe):
-            exe = check_exe
-        if exe == False:
-            print("ERROR: unable to find JEDI executable")
-        self.jedi_exe = exe
+        # handeled by CMAKE
+        # exe = False
+        # check_exe = self.jedi_build_dir + self.jedi_exe
+        # if os.path.exists(check_exe):
+        #     exe = check_exe
+        # check_exe = self.jedi_build_dir + 'bin/' + self.jedi_exe
+        # if os.path.exists(check_exe):
+        #     exe = check_exe
+        # if exe == False:
+        #     print("ERROR: unable to find JEDI executable")
+        # self.jedi_exe = exe
         # self.create_dir_structure()
 
     # def create_dir_structure(self):
@@ -158,11 +233,11 @@ class workflow:
     def read_yaml(self):
         f = open(self.yaml, 'r')
         self.setup = yaml.safe_load(f)
+        self.read_yaml_time()
         self.read_jedi_yaml()
         self.read_wrf_hydro_yaml()
-        # self.read_yaml_dirs()
+        self.read_yaml_dirs()
         self.read_yaml_file_names()
-        self.read_yaml_time()
         # self.read_yaml_command() Don't need??
         # ens_size = int(config['ensemble']['size'])
         f.close()
@@ -178,11 +253,12 @@ class workflow:
     def read_jedi_yaml(self):
         self.jedi_yaml = self.setup['experiment']['jedi']['yaml']
         f = open(self.jedi_yaml, 'r')
-        self.jedi_setup = yaml.safe_load(f)
-        background = self.jedi_setup['cost function']['background']
+        jedi_setup = yaml.safe_load(f)
+        background = jedi_setup['cost function']['background']
         # the following two should be restart files
-        self.input_lsm_f = background['filename_lsm']
-        self.input_hydro_f = background['filename_hydro']
+        self.lsm_file = filename(background['filename_lsm'], self.model_dt)
+        # self.hydro_f = filename(background['filename_hydro'], self.model_dt)
+
 
         jedi = self.setup['experiment']['jedi']
         self.jedi_build_dir = check_path(jedi['build_dir'])
@@ -192,23 +268,24 @@ class workflow:
         self.jedi_output_dir = check_path(jedi['output_dir'])
         self.jedi_exe = jedi['exe']
 
-        print("ARTLESS: tmp jedi_output_dir")
+        print("TODO/NOTE: tmp jedi_output_dir")
         # self.jedi_output_dir = check_path(jedi['output'])
         f.close()
 
     def read_yaml_command(self):
         python = '/glade/u/apps/opt/conda/envs/npl/bin/python3 '
         command = self.setup['experiment']['command']
-        # ARTLESS
+        # TODO: double check '/' needed
         self.command = python +  self.jedi_output_dir+'/'+str(command)
 
     def read_yaml_dirs(self):
-        dirs = self.setup['experiment']['dirs']
-        self.base_dir = dirs['output_dir']
-        # self.build_dir = dirs['build_dir']
-        self.experiment_dir = dirs['experiment_dir']
-        self.experiment_output_dir = dirs['experiment_output_dir']
-        self.init_ensemble_dir = dirs['initial_ensemble_dir']
+        self.workflow_work_dir = self.setup['experiment']['workflow_work_dir']
+        # dirs = self.setup['experiment']['dirs']
+        # self.base_dir = dirs['output_dir']
+        # # self.build_dir = dirs['build_dir']
+        # self.experiment_dir = dirs['experiment_dir']
+        # self.experiment_output_dir = dirs['experiment_output_dir']
+        # self.init_ensemble_dir = dirs['initial_ensemble_dir']
 
     def read_yaml_file_names(self):
         f_names = self.setup['experiment']['file_names']
@@ -221,7 +298,7 @@ class workflow:
         self.end_time = datetime.datetime.strptime(time['end_time'],
                                                    '%Y-%m-%d_%H:%M:%S')
         self.assim_window_hr = time['assim_window']['hours']
-        self.model_ts = datetime.timedelta(hours=time['advance_model_hours'])
+        self.model_dt = datetime.timedelta(hours=time['advance_model_hours'])
         # skip_missing_obs_hours = time['skip_missing_obs_hours']
         # skip_missing_obs_days = time['skip_missing_obs_days']
 
@@ -242,14 +319,19 @@ class workflow:
         print("increment exe", self.increment_exe)
         print("FIX AT SOME POINT")
 
-    def end(self):
-        pprint("Exiting Workflowpy")
-
     def print_setup(self):
         pprint("Directories")
-        print("Working dir: ", self.jedi_working_dir) #self.run_dir) # ARTLESS
-        print("Output dir: ", self.jedi_output_dir) # self.obs_dir) ARTLESS
+        print("Working dir: ", self.jedi_working_dir) #self.run_dir) # CHANGE
+        print("Output dir: ", self.jedi_output_dir) # self.obs_dir)  # BACK?
         # print("ens_size: ", self.ens_size)
+
+
+    def write_jedi_yaml(self, yaml_output):
+        f = open(self.jedi_yaml, 'w')
+        yaml.dump(yaml_output, f)
+        f.close()
+
+
 
 
     # Arguments to this script.
